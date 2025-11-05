9e0b7cb8744afbea3cc26f785fce395fec25fddd
Kim Hauser <knhauser1s@gmail.com>
2025-11-04 22:32:46 -0600

    Exercise complete

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+//Task 1: Attendee Badge:
 
+function generateBadge(Name, Role) {
+    return(`Name: ${Name}, Role: ${Role}`);
+}
+
+console.log(generateBadge("Kimi", "Product Administrator"));
+
+//Task 2: Calculating Cost:
+
+function calculateCost(numberOfAttendees, costPerAttendee) {
+    if (numberOfAttendees > 100) {
+        return (costPerAttendee * numberOfAttendees) * 0.90;
+     } 
+     else {
+       return costPerAttendee * numberOfAttendees;
+    }
+}
+
+console.log(calculateCost(120,25));
+
+//Task 3: Validating Email:
+
+function validateEmail (email) {
+    let checkForAt = false;
+    let checkForDot = false;
+    let emailString = email;
+    if (emailString.includes("@")) {
+        checkForAt = true;
+    }
+    if (emailString.includes(".")) {
+        checkForDot = true;
+    }
+    return checkForAt && checkForDot;
+}
+
+console.log(validateEmail('alice@email.com'));
+console.log(validateEmail('alice.'));
+console.log(validateEmail('Alice'))
